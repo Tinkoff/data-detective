d97b1574f5f86ff011d6aad15523e73f53bddeb8
Maxim Belousov <mebelousov@ya.ru>
2021-11-16 10:58:49 +0300

    MG-2374 Reorganize readme, rename services, fix airflow version for 2.1.* (#6)
    * Rename docs in s3_work.py
    
    * Delete ROOT_DIR
    
    * Fix version airflow ~2.1
    
    * Remove work from connection names
    
    * Remove Contributing part
    
    * Move release to docs
    
    * Add classifiers, keywords and repository link
    
    * Format description in README.MD

--- mg-airflow/docker/init-connections.sh
+++ mg-airflow/docker/init-connections.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Здесь нужно задать все подключения ко всем test/dev серверам
-airflow connections add --conn-uri 'postgres://airflow:airflow@pgwork:5432' pg
+airflow connections add --conn-uri 'postgres://airflow:airflow@pg:5432' pg
 airflow connections add --conn-uri 'ftp://airflow:airflow@ssh_service:22' ssh_service
-airflow connections add --conn-type s3 --conn-extra "{\"aws_access_key_id\": \"accessKey1\", \"aws_secret_access_key\":\"verySecretKey1\", \"host\":\"http://s3work:4566\"}" s3
+airflow connections add --conn-type s3 --conn-extra "{\"aws_access_key_id\": \"accessKey1\", \"aws_secret_access_key\":\"verySecretKey1\", \"host\":\"http://s3:4566\"}" s3
---
+++ tools/doc-site/docs/mg-airflow/release.md
@@ -0,0 +1,6 @@
+# How to release mg-airflow
+
+Create a release with tag like "mg-airflow/*" in repo.
+Action "Publish mg-airflow" will be triggered.
+
+Package version is extracted from `pyproject.toml`.
