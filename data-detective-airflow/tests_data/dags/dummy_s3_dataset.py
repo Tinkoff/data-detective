08f36f101feaa3de97de07aba50d499639f1b1e5
Maxim Belousov <mebelousov@ya.ru>
2021-11-23 17:59:49 +0300

    Починить тест для dummy_s3

--- data-detective-airflow/tests_data/dags/dummy_s3_dataset.py
+++ data-detective-airflow/tests_data/dags/dummy_s3_dataset.py
@@ -4,14 +4,14 @@ from pandas._libs.tslibs.timestamps import Timestamp
 dataset = {
     'list_bucket': DataFrame(
         [
-            ['dd-airflow', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"a6dbd29200b9daa7712a89c8656a7860"', 11, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}],
-            ['dd-airflow.txt', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"ef6765cbc100e8ca80ba73e70bb71d57"', 15, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}]
+            ['dd-airflow', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"e9ce7bfc6c70c80bb03e168fb64f925b"', 11, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}],
+            ['dd-airflow.txt', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"bcb6d17ea497aec5b15cc12eeb0d2f49"', 15, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}]
         ],
         columns=['key', 'lastmodified', 'etag', 'size', 'storageclass', 'owner']),
     's3_dump': DataFrame(
         [
-            ['dd-airflow', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"a6dbd29200b9daa7712a89c8656a7860"', 11, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}, b'dd-airflow\n'],
-            ['dd-airflow.txt', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"ef6765cbc100e8ca80ba73e70bb71d57"', 15, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}, b'dd-airflow.txt\n']
+            ['dd-airflow', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"e9ce7bfc6c70c80bb03e168fb64f925b"', 11, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}, b'dd-airflow\n'],
+            ['dd-airflow.txt', Timestamp('2020-11-09 08:29:03+0000', tz='tzlocal()'), '"bcb6d17ea497aec5b15cc12eeb0d2f49"', 15, 'STANDARD', {'DisplayName': 'webfile', 'ID': '75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a'}, b'dd-airflow.txt\n']
         ],
         columns=['key', 'lastmodified', 'etag', 'size', 'storageclass', 'owner', 'response']),
     'decode_response': DataFrame(
