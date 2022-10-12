1d705aaeb3e8a3540520ac6fc79c5a5adc2b3e52
Ivan Kanashov <i.kanashov@tinkoff.ru>
2022-10-12 11:02:48 +0500

    feat: add create_dag_run to dag_generator tests

--- data-detective-airflow/tests/dag_generator/results/test_pgresult.py
+++ data-detective-airflow/tests/dag_generator/results/test_pgresult.py
@@ -5,6 +5,7 @@ from pandas import DataFrame
 
 from data_detective_airflow.constants import PG_CONN_ID
 from data_detective_airflow.dag_generator import ResultType, WorkType
+from data_detective_airflow.test_utilities import create_or_get_dagrun, get_template_context
 
 
 @allure.feature('Results')
@@ -14,7 +15,9 @@ from data_detective_airflow.dag_generator import ResultType, WorkType
                            WorkType.WORK_PG.value,
                            PG_CONN_ID)],
                          indirect=True, ids=['pg-result-write'])
-def test_write(test_dag, dummy_task, context):
+def test_write(test_dag, dummy_task):
+    create_or_get_dagrun(test_dag, dummy_task)
+    context = get_template_context(dummy_task)
     work = test_dag.get_work(test_dag.work_type, test_dag.conn_id)
     work.create(context)
     res = test_dag.get_result(operator=dummy_task,
@@ -39,7 +42,9 @@ def test_write(test_dag, dummy_task, context):
                            WorkType.WORK_PG.value,
                            PG_CONN_ID)],
                          indirect=True, ids=['pg-result-read'])
-def test_read(test_dag, dummy_task, context):
+def test_read(test_dag, dummy_task):
+    create_or_get_dagrun(test_dag, dummy_task)
+    context = get_template_context(dummy_task)
     test_dag.get_work(test_dag.work_type, test_dag.conn_id).create(context)
     res = test_dag.get_result(operator=dummy_task, result_name='test',
                               result_type=test_dag.result_type,
