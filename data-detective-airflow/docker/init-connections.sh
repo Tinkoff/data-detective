aabc47c713e54b66cbc0efba90c36d5e8c95e20d
Ivan Kanashov <i.kanashov@tinkoff.ru>
2022-10-12 10:45:27 +0500

    refactor: update aws connections

--- data-detective-airflow/docker/init-connections.sh
+++ data-detective-airflow/docker/init-connections.sh
@@ -3,4 +3,4 @@
 # Here it is necessary to set all connections to test/dev servers
 airflow connections add --conn-uri 'postgres://airflow:airflow@pg:5432' pg
 airflow connections add --conn-uri 'ftp://airflow:airflow@ssh_service:22' ssh_service
-airflow connections add --conn-type s3 --conn-extra "{\"aws_access_key_id\": \"accessKey1\", \"aws_secret_access_key\":\"verySecretKey1\", \"host\":\"http://s3:4566\"}" s3
+airflow connections add --conn-type aws --conn-extra "{\"aws_access_key_id\": \"accessKey1\", \"aws_secret_access_key\":\"verySecretKey1\", \"endpoint_url\":\"http://s3:4566\"}" s3
