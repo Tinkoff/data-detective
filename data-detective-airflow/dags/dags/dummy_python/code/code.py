1b42696596366d51f783765433c3fe27b9a96004
SergeyBychkov <coursera.org@mail.ru>
2023-06-02 17:55:04 +0300

    fix: lint

--- data-detective-airflow/dags/dags/dummy_python/code/code.py
+++ data-detective-airflow/dags/dags/dummy_python/code/code.py
@@ -6,12 +6,38 @@ from data_detective_airflow.operators.transformers import Append, PyTransform
 
 
 def fill_dag(tdag: TDag):
-    DBDump(task_id="test1", conn_id=PG_CONN_ID, sql="/code/test1.sql", dag=tdag)
+    DBDump(
+        task_id="test1",
+        conn_id=PG_CONN_ID,
+        sql="/code/test1.sql",
+        dag=tdag,
+    )
 
-    DBDump(task_id="test2", conn_id=PG_CONN_ID, sql="/code/test1.sql", dag=tdag)
+    DBDump(
+        task_id="test2",
+        conn_id=PG_CONN_ID,
+        sql="/code/test1.sql",
+        dag=tdag,
+    )
 
-    PyTransform(task_id="transform", source=["test2"], transformer_callable=lambda _context, df: df, dag=tdag)
+    PyTransform(
+        task_id="transform",
+        source=["test2"],
+        transformer_callable=lambda _context, df: df,
+        dag=tdag,
+    )
 
-    Append(task_id="append_all", source=["transform", "test1"], dag=tdag)
+    Append(
+        task_id="append_all",
+        source=["transform", "test1"],
+        dag=tdag,
+    )
 
-    PgSCD1(task_id="sink", source=["append_all"], conn_id=PG_CONN_ID, table_name="test2", key=["test"], dag=tdag)
+    PgSCD1(
+        task_id="sink",
+        source=["append_all"],
+        conn_id=PG_CONN_ID,
+        table_name="test2",
+        key=["test"],
+        dag=tdag,
+    )
