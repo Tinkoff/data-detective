2f7a54535801712eac685a836fdb43922509f754
SergeyBychkov <coursera.org@mail.ru>
2023-06-02 14:36:02 +0300

    feat: bump python libs version

--- data-detective-airflow/dags/dags/dummy/code/code.py
+++ data-detective-airflow/dags/dags/dummy/code/code.py
@@ -1,14 +1,27 @@
 import yaml
-from pandas import DataFrame
+from airflow.utils.context import Context
+from pandas import DataFrame, concat
 
 
-def val_translate(context, in_df: DataFrame, file_name: str) -> DataFrame:
-    task = context.get('task')
+def val_translate(context: Context, in_df: DataFrame, file_name: str) -> DataFrame:
+    """Translate values
+    :param context: Airflow context
+    :param in_df: incoming df
+    :param file_name: translate file
+    :return:
+    """
+    task = context.get("task")
     out_df = in_df.copy()
-    with open(f'{task.dag.etc_dir}/{file_name}', 'r', encoding='utf-8') as cfg:
+    with open(f"{task.dag.etc_dir}/{file_name}", "r", encoding="utf-8") as cfg:  # type: ignore
         config = yaml.safe_load(cfg)
-    out_df['test'] = out_df.apply(
-        lambda row: config[row['test']],
-        axis=1
-    )
+    out_df["test"] = out_df.apply(lambda row: config[row["test"]], axis=1)
     return out_df
+
+
+def append_dfs(_context: Context, *sources: DataFrame) -> DataFrame:
+    """Append several dfs in one
+    :param _context: Airflow context
+    :param sources: dfs
+    :return:
+    """
+    return concat([*sources])
--- data-detective-airflow/data_detective_airflow/operators/transformers/append.py
+++ data-detective-airflow/data_detective_airflow/operators/transformers/append.py
@@ -1,35 +1,30 @@
-from airflow import AirflowException
+from typing import Any, Optional
+
+from pandas import concat
 
 from data_detective_airflow.operators.tbaseoperator import TBaseOperator
 
 
 class Append(TBaseOperator):
-    """Merge multiple objects into one
-    The object must have an append method
+    """Merge multiple DataFrames into one
 
     :param source: Source
+    :param append_options: Additional options for pandas.concat() method
     :param kwargs: Additional params for the TBaseOperator
     """
 
-    ui_color = '#8f75d1'
+    ui_color = "#8f75d1"
 
-    def __init__(self, source: list[str], **kwargs):
+    def __init__(self, source: list[str], append_options: Optional[dict[str, Any]] = None, **kwargs):
         super().__init__(**kwargs)
         self.source = source
+        self.append_options = append_options or {}
         for src in self.source:
             self.dag.task_dict[src] >> self  # pylint: disable=pointless-statement
 
     def execute(self, context):
-        result = None
-        self.log.info('Start appending')
-        for src in self.source:
-            read_result = self.dag.task_dict[src].result.read(context)
-            if result is None:
-                result = read_result
-                if 'append' not in dir(result):
-                    raise AirflowException('Object should have "append" method.')
-                continue
-            result = result.append(read_result, sort=False)
-        self.log.info('Writing pickle result')
-        self.result.write(result, context)
-        self.log.info('Finish')
+        self.log.info("Start appending...")
+        result = [self.dag.task_dict[src].result.read(context) for src in self.source]
+        self.log.info("Writing pickle result")
+        self.result.write(concat(result, **self.append_options), context)
+        self.log.info("Finish")
